35655d9f328799bc9a880fb3fc7d961edd7aebcd
JessieSun <46373012+JessieSun-vibe@users.noreply.github.com>
2026-06-24 22:53:16 +0800

    chore: add source files in src/parser

---
+++ src/parser/parser.ts
@@ -0,0 +1,102 @@
+import type { Moment } from "moment";
+import { isNotVoid } from "typed-assert";
+
+import {
+  strictTimestampAnywhereInLineRegExp,
+  looseTimestampAtStartOfLineRegExp,
+} from "../regexp";
+import { getDiffInMinutes } from "../util/moment";
+
+import { parseTimestamp } from "./timestamp";
+
+function execTimestampPatterns(line: string) {
+  const trimmed = line.trim();
+
+  return (
+    // todo: never use exec
+    looseTimestampAtStartOfLineRegExp.exec(trimmed) ||
+    strictTimestampAnywhereInLineRegExp.exec(trimmed)
+  );
+}
+
+export function testTimestampPatterns(line: string) {
+  const trimmed = line.trim();
+
+  return (
+    looseTimestampAtStartOfLineRegExp.test(trimmed) ||
+    strictTimestampAnywhereInLineRegExp.test(trimmed)
+  );
+}
+
+export function replaceOrPrependTimestamp(line: string, timestamp: string) {
+  if (looseTimestampAtStartOfLineRegExp.test(line)) {
+    return line.replace(looseTimestampAtStartOfLineRegExp, timestamp);
+  }
+
+  if (strictTimestampAnywhereInLineRegExp.test(line)) {
+    return line.replace(strictTimestampAnywhereInLineRegExp, timestamp);
+  }
+
+  return `${timestamp} ${line}`;
+}
+
+export function getTimeFromLine({ line, day }: { line: string; day: Moment }) {
+  const match = execTimestampPatterns(line);
+
+  if (!match?.groups) {
+    return null;
+  }
+
+  const {
+    groups: { start, end },
+  } = match;
+
+  isNotVoid(
+    start,
+    "Expected to find 'start' group on a timestamp regexp match",
+  );
+
+  const startTime = parseTimestamp(start, day);
+
+  let durationMinutes: number | undefined;
+
+  if (end) {
+    const endTime = parseTimestamp(end, day);
+
+    // todo: handle edge, use default duration
+    if (endTime.isAfter(startTime)) {
+      durationMinutes = getDiffInMinutes(endTime, startTime);
+    } else {
+      durationMinutes = getDiffInMinutes(
+        startTime,
+        endTime.clone().add(1, "day"),
+      );
+    }
+  }
+
+  return {
+    startTime,
+    durationMinutes,
+  };
+}
+
+export function compareTimestamps(a: string, b: string) {
+  const now = window.moment();
+
+  const aTime = getTimeFromLine({ line: a, day: now });
+  const bTime = getTimeFromLine({ line: b, day: now });
+
+  if (!aTime && !bTime) {
+    return 0;
+  }
+
+  if (!aTime) {
+    return 1;
+  }
+
+  if (!bTime) {
+    return -1;
+  }
+
+  return aTime.startTime.diff(bTime.startTime);
+}
