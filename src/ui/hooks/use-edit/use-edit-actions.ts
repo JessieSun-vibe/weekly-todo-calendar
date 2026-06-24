51927d6f08ac6082c0242176ca3956ec14783969
JessieSun <46373012+JessieSun-vibe@users.noreply.github.com>
2026-06-24 23:38:17 +0800

    chore: add source files in src/ui/hooks/use-edit 1-4

---
+++ src/ui/hooks/use-edit/use-edit-actions.ts
@@ -0,0 +1,60 @@
+import { get, type Readable, type Writable } from "svelte/store";
+
+import { vibrationDurationMillis } from "../../../constants";
+import type { LocalTask } from "../../../task-types";
+import type { OnUpdateFn } from "../../../types";
+
+import type { EditOperation } from "./types";
+
+interface UseEditActionsProps {
+  baselineTasks: Writable<LocalTask[]>;
+  editOperation: Writable<EditOperation | undefined>;
+  tasksWithPendingUpdate: Readable<LocalTask[]>;
+  onUpdate: OnUpdateFn;
+}
+
+export function useEditActions({
+  editOperation,
+  baselineTasks,
+  tasksWithPendingUpdate,
+  onUpdate,
+}: UseEditActionsProps) {
+  function startEdit(operation: EditOperation) {
+    navigator.vibrate?.(vibrationDurationMillis);
+    editOperation.set(operation);
+  }
+
+  function cancelEdit() {
+    editOperation.set(undefined);
+  }
+
+  async function confirmEdit() {
+    const currentOperation = get(editOperation);
+
+    if (currentOperation === undefined) {
+      return;
+    }
+
+    const oldBase = get(baselineTasks);
+    const currentTasks = get(tasksWithPendingUpdate);
+
+    baselineTasks.set(currentTasks);
+    editOperation.set(undefined);
+
+    const succeeded = await onUpdate(
+      oldBase,
+      currentTasks,
+      currentOperation.mode,
+    );
+
+    if (!succeeded) {
+      baselineTasks.set(oldBase);
+    }
+  }
+
+  return {
+    startEdit,
+    confirmEdit,
+    cancelEdit,
+  };
+}
