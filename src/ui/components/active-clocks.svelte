097072ad7000fa209d2e4137aa505f08dab0999e
JessieSun <46373012+JessieSun-vibe@users.noreply.github.com>
2026-06-24 23:08:47 +0800

    chore: add source files in src/ui/components 1-4

---
+++ src/ui/components/active-clocks.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+  import { Play, Trash, Hourglass, File, Square } from "lucide-svelte";
+  import { isNotVoid } from "typed-assert";
+
+  import { getObsidianContext } from "../../context/obsidian-context";
+  import { currentTimeSignal } from "../../global-store/current-time";
+  import { settings } from "../../global-store/settings";
+  import { selectActiveLogEntries } from "../../redux/index/index-slice";
+  import { runWithNoticeOnError } from "../../service/list-item-entry-editor";
+  import type { LocalTask } from "../../task-types";
+  import * as m from "../../util/moment";
+  import { getDiffInMinutes } from "../../util/moment";
+  import { createActiveClockMenu } from "../active-clock-menu";
+
+  import BlockList from "./block-list.svelte";
+  import LocalTimeBlock from "./local-time-block.svelte";
+  import Pill from "./pill.svelte";
+  import Properties from "./properties.svelte";
+  import Selectable from "./selectable.svelte";
+
+  const { workspaceFacade, taskEntryEditor, useSelector } =
+    getObsidianContext();
+
+  const activeLogRecords = useSelector(selectActiveLogEntries);
+  // todo: duplication?
+  const activeLogRecordsCompat = $derived(
+    activeLogRecords.current.map((it) => ({
+      ...it,
+      durationMinutes: getDiffInMinutes(
+        it.startTime,
+        currentTimeSignal.current,
+      ),
+    })),
+  );
+</script>
+
+<BlockList list={activeLogRecordsCompat}>
+  {#snippet match(task: LocalTask)}
+    <Selectable
+      onSecondarySelect={(event) =>
+        createActiveClockMenu({
+          event,
+          task,
+          taskEntryEditor,
+          workspaceFacade,
+        })}
+    >
+      {#snippet children({ use, onpointerup, state })}
+        <LocalTimeBlock
+          isActive={state === "secondary"}
+          {onpointerup}
+          {task}
+          {use}
+        >
+          {#snippet bottomDecoration()}
+            <Properties>
+              {#if task.location?.path}
+                <Pill
+                  key={File}
+                  onpointerup={() => {
+                    isNotVoid(task.location);
+
+                    return workspaceFacade.revealLineInFile(
+                      task.location.path,
+                      task.location.position.start.line,
+                    );
+                  }}
+                  value={task.location.path.replace(/\.md$/, "")}
+                />
+              {/if}
+              <Pill
+                key={Play}
+                value={task.startTime.format($settings.timestampFormat)}
+              />
+              <Pill
+                key={Hourglass}
+                value={m
+                  .fromDiff(task.startTime, currentTimeSignal.current)
+                  .format($settings.timestampFormat)}
+              />
+
+              <Pill
+                key={Square}
+                onpointerup={async () => {
+                  isNotVoid(task.location);
+
+                  await runWithNoticeOnError(
+                    taskEntryEditor.clockOutAtLocation(task.location),
+                  );
+                }}
+                value="Stop"
+              />
+
+              <Pill
+                key={Trash}
+                onpointerup={async () => {
+                  isNotVoid(task.location);
+
+                  await runWithNoticeOnError(
+                    taskEntryEditor.cancelClockAtLocation(task.location),
+                  );
+                }}
+                value="Cancel"
+              />
+            </Properties>
+          {/snippet}
+        </LocalTimeBlock>
+      {/snippet}
+    </Selectable>
+  {/snippet}
+</BlockList>
---
+++ src/ui/components/block-list.svelte
@@ -0,0 +1,28 @@
+<script generics="T extends { id?: string }" lang="ts">
+  import type { Snippet } from "svelte";
+  import { slide } from "svelte/transition";
+
+  import { createSlide } from "./defaults";
+
+  // eslint doesn't know about Svelte generics
+  // eslint-disable-next-line no-undef
+  const { list, match }: { match: Snippet<[T]>; list: Array<T> } = $props();
+</script>
+
+{#if list.length > 0}
+  <div
+    class="search-results-scroller"
+    transition:slide={createSlide({ axis: "y" })}
+  >
+    {#each list as foundTimeBlock, index (foundTimeBlock.id || index)}
+      {@render match(foundTimeBlock)}
+    {/each}
+  </div>
+{/if}
+
+<style>
+  .search-results-scroller {
+    overflow-y: auto;
+    padding: var(--size-4-1) var(--size-4-2);
+  }
+</style>
